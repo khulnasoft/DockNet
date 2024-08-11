74bbb8b0a4282dbb252602b4a45851067f972fe4
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-08-11 12:34:54 +0200

    update

--- backend/setup.py
+++ backend/setup.py
@@ -53,7 +53,7 @@ setup(
     install_requires=[
         "typer",  # TODO: remove typer?
         "pydantic[dotenv,email]",
-        "readyapi==0.75.2",
+        "readyapi>=0.75.2",
         "requests",
         # Better logging
         "loguru",
